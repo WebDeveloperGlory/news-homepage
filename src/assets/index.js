6da9cce37a9a0753810d49d316c2a5b2289900d6
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-13 03:32:49 -0800

    file and folder structure set-up and assets export

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg'
+import menu from './menu.svg'
+import menu_close from './menu-close.svg'
+import gaming_growth from './gaming-growth.jpg'
+import retro_pcs from './retro-pcs.jpg'
+import top_laptops from './top-laptops.jpg'
+import web_3_desktop from './web-3-desktop.jpg'
+import web_3_mobile from './web-3-mobile.jpg'
+
+export {
+    logo,
+    menu,
+    menu_close,
+    gaming_growth,
+    retro_pcs,
+    top_laptops,
+    web_3_desktop,
+    web_3_mobile
+}
