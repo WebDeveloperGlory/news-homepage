2dde87b331b512b5f1fc20f7c4e471d5f9bf0cda
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-13 12:31:04 -0800

    complete navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <section className="nav">
+      <section className="navSec">
         <Navbar />
       </section>
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,17 @@ import { logo } from '../assets'
 import './css/Navbar.css'
 
 const Navbar = () => {
+    const navLinks = navbar.map((link, i) => (
+      <li key={`${link}-${i}`}>
+        <a className='navLink'>{link}</a>
+      </li>    
+    ));
   return (
-    <div>
+    <div className='nav'>
         <img src={logo} alt="logo" />
+        <ul>
+          {navLinks}
+        </ul>
     </div>
   )
 }
