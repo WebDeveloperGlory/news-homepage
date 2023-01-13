4a15ed5c660c33a70a8ecbf91421505aff2f6a02
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-13 03:45:59 -0800

    get data contant

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+export const data = {
+    navbar: [ "Home", "New", "Popular", "Trending", "Categories" ],
+    hero: {
+        title: "The Bright Future of Web 3.0?",
+        content: "We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?",
+        btn: "Read More"
+    },
+    new: {
+        title: "New",
+        content: [
+            {
+                title: "Hydrogen VS Electric Cars",
+                content: "Will hydrogen-fueled cars ever catch up to EVs?"
+            },
+            {
+                title: "The Downsides of AI Artistry",
+                content: "What are the possible adverse effects of on-demand AI image generation?"
+            },
+            {
+                title: "Is VC Funding Drying Up?",
+                content: "Private funding by VC firms is down 50% YOY. We take a look at what that means."
+            }
+        ]
+    },
+    popular: {
+        content: [
+            {
+                number: "01",
+                title: "Reviving Retro PCs",
+                content: "What happens when old PCs are given modern upgrades?"
+            },
+            {
+                number: "02",
+                title: "Top 10 Laptops of 2022",
+                content: "Our best picks for various needs and budgets."
+            },
+            {
+                number: "01",
+                title: "The Growth of Gaming",
+                content: "How the pandemic has sparked fresh opportunities."
+            }
+        ]
+    }
+}
