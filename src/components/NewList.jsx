d886a7743134f9191ef037b9408962e53255d095
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-14 11:10:21 -0800

    basic newList component setup

--- src/components/NewList.jsx
+++ src/components/NewList.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NewCard } from './'
+import { newList } from '../constants'
 import './css/NewList.css'
 
 function NewList() {
+    const title = newList.title;
   return (
-    <div>NewList</div>
+    <div className='newList'>
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <div className="list">
+        <NewCard />
+      </div>
+    </div>
   )
 }
 
