e07a95b1100d295505908519f91b83ce8aa9851b
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-01-14 10:55:48 -0800

    design hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { web_3_desktop } from '../assets'
+import { hero } from '../constants'
 import './css/Hero.css'
 
 const Hero = () => {
+    const title = hero.title;
+    const txt = hero.content;
+    const btnTxt = hero.btn;
   return (
-    <div>Hero</div>
+    <div className='heroContainer'>
+      <div className="topSection">
+        <img src={web_3_desktop} alt="web-3 image" />
+      </div>
+      <div className="bottomSection">
+        <div className="title">
+          <h1>{title}</h1>
+        </div>
+        <div className="text">
+          <p>{txt}</p>
+          <button>{btnTxt}</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
